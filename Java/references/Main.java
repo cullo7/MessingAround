8144d38f2acd820f29adfd982c0bb3b203192f0b
Aiden Cullo <aidencullo@gmail.com>
2022-11-30 09:48:32 -0500

    dynamic memory allocation testing

--- Java/references/Main.java
+++ Java/references/Main.java
@@ -1,12 +1,28 @@
-import refpkg.Reference;
-
 class Main {
 
+    public static void test(Node n){
+	Node n1 = new Node(2);
+	n.next = n1;
+    }
+
+    public static class Node{
+	int val;
+	Node next;
+	Node(int num){ val = num;}
+    }
+
+    public static void print(Node n){
+	while(n != null){
+	    System.out.println(n.val);
+	    n = n.next;
+	}
+    }
+    
     public static void main(String args[]){
 
-	Reference r = new Reference();
-	Reference.changeVal(r);
-	r.printCount();	
+	Main.Node n = new Main.Node(1);
+	Main.test(n);
+	Main.print(n);
 	
     }
 }
